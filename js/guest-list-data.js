bff2909c0ad55e9b6f3cc94b34244ab313e1668f
alojua <juan.alonso@staempfli.com>
2018-05-26 11:03:18 +0200

    Update guest list

--- js/guest-list-data.js
+++ js/guest-list-data.js
@@ -1,33 +1,56 @@
 var guestList = [];
-guestList['aloala'] = {first: "Alvaro", second: "Alicia"};
-guestList['jarala'] = {first: "Javier", second: "Almudena"};
+
+// Common
+guestList['maapaa'] = {first: "Mama", second: "Papa"};
+
+// Nuria
+guestList['lusdei'] = {first: "Luis", second: "Dei"};
+guestList['eneher'] = {first: "Enrique", second: "Henar"};
+guestList['beajes'] = {first: "Bea", second: "Jesus"};
+guestList['hel'] = {first: "Hele"};
+guestList['maapao'] = {first: "Marga", second: "Pablo"};
+guestList['esr'] = {first: "Esther"};
+guestList['maario'] = {first: "Maria", second: "Ricardo"};
+guestList['cis'] = {first: "Cris"};
+guestList['isa'] = {first: "Isa"};
+guestList['naa'] = {first: "Natalia"};
+guestList['ivnlei'] = {first: "Ivan", second: "Leti"};
+guestList['goono'] = {first: "Gonzalo", second: "Monica"};
+guestList['massha'] = {first: "Marcos", second: "Sheila"};
+guestList['diosaa'] = {first: "Diego", second: "Sara"};
+guestList['sailaa'] = {first: "Santi", second: "Laura"};
+guestList['enerea'] = {first: "Enrique", second: "Rebeca"};
+guestList['cassaa'] = {first: "Carlos", second: "Sara"};
+guestList['sauloe'] = {first: "Samu", second: "Lore"};
+guestList['nis'] = {first: "Nines"};
+
+// Juan
+guestList['rioala'] = {first: "Rigo", second: "Alicia"};
+guestList['meialu'] = {first: "Medi", second: "Almu"};
 guestList['roimaa'] = {first: "Rodri", second: "Maria"};
-guestList['naovea'] = {first: "Nacho", second: "Veronica"};
-guestList['pozcra'] = {first: "Poliz", second: "Cristina"};
+guestList['naoveo'] = {first: "Nacho", second: "Vero"};
+guestList['pozcrs'] = {first: "Poliz", second: "Cris"};
 guestList['dailei'] = {first: "Dani", second: "Leti"};
 guestList['edulua'] = {first: "Edu", second: "Lucia"};
 guestList['aansoa'] = {first: "Aaron", second: "Sonia"};
-guestList['lusdei'] = {first: "Luis", second: "Dei"};
-guestList['eneher'] = {first: "Enrique", second: "Henar"};
+guestList['chebea'] = {first: "Chule", second: "Bea"};
+guestList['manpir'] = {first: "Martin", second: "Pilar"};
+guestList['maoroo'] = {first: "Mario", second: "Rocio"};
+guestList['tae'] = {first: "Tanque"};
+guestList['cesale'] = {first: "Cesar", second: "Ale"};
+guestList['wiy'] = {first: "Willy"};
+guestList['poo'] = {first: "Pozo"};
+guestList['saa'] = {first: "Santiaga"};
+guestList['tinesr'] = {first: "Tinin", second: "Esther"};
+guestList['xaiace'] = {first: "Xavi", second: "Sara"};
 
-// todo
-guestList['x'] = {first: "Sergio", second: ""};
-guestList['x'] = {first: "David", second: ""};
-guestList['x'] = {first: "Irene", second: ""};
-guestList['x'] = {first: "Mario", second: ""};
-guestList['x'] = {first: "Tanque", second: ""};
-guestList['x'] = {first: "Pozo", second: ""};
-guestList['x'] = {first: "Tinin", second: ""};
-guestList['x'] = {first: "Cesar", second: ""};
-guestList['x'] = {first: "Carlitos", second: ""};
-guestList['x'] = {first: "Lopez", second: ""};
-guestList['x'] = {first: "Alfonso", second: ""};
-guestList['x'] = {first: "Javi Primo", second: ""};
-guestList['x'] = {first: "Irene", second: ""};
-guestList['x'] = {first: "Lucia", second: ""};
-guestList['x'] = {first: "Alberto", second: ""};
-guestList['x'] = {first: "Julia", second: ""};
-guestList['ivnace'] = {first: "Ivan", second: "Acompañante"};
-guestList['wiyace'] = {first: "Willy", second: "Acompañante"};
-guestList['xaiace'] = {first: "Xavi", second: "Acompañante"};
-guestList['alxace'] = {first: "Alex", second: "Acompañante"};
+guestList['alx'] = {first: "Alex"};
+guestList['caseii'] = {first: "Carlitos", second: "Eirini"};
+guestList['lozmaa'] = {first: "Lopez", second: "Mayara"};
+
+guestList['ire'] = {first: "Irene"};
+guestList['alnveo'] = {first: "Alfon", second: "Vero"};
+guestList['jai'] = {first: "Javi"};
+guestList['luades'] = {first: "Lucia", second: "Denis"};
+guestList['alo'] = {first: "Alberto"};
+//guestList['jua'] = {first: "Julia", second: ""};
